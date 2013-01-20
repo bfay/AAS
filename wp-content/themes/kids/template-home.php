5d8a2c4f73278c0b6087be3914fb0528f4c5a44b
Byron Fay <bfay_git@me.com>
2013-01-20 14:41:04 -0500

    updated home.php

--- wp-content/themes/kids/template-home.php
+++ wp-content/themes/kids/template-home.php
@@ -9,88 +9,83 @@
 
 <?php get_header(); ?>
 
-<div id="content" class="home">
-<?= do_shortcode('[layerslider id="1"]'); ?>
-</div>
-    <div class="opt-in"><img src="http://www.allergiesatschool.com/wp-content/themes/kids/images/opt-in.png" title="opt-in form"/>	
-    </div>
-
-    <div id="featured">
-        <div class="wrap">
-               
-                <div class="<?php echo ($featured_widget_on ? 'c-8':'c-12') ?>">
-                    <div class="page">
-                        <h2><a title="<?php the_title_attribute(); ?>" href="<?php echo get_permalink($featured_item->ID); ?>"><?php echo strtoupper($featured_item->post_title); ?></a></h2>
-                        <?php if($featured_section_full) :?>
-                           <?php echo apply_filters('the_content', $featured_item->post_content); ?> 
-                        <?php else : ?>
-                            <p><?php echo mls_the_excerpt_max_charlength(strip_shortcodes($featured_item->post_content), 250, true); ?></p>
-                        <?php endif;?> 
-                        
-                        <?php if($featured_readmore_link_show) : ?>
-                        <a href="<?php echo get_permalink($featured_item_id); ?>" class="read-more"><?php echo $readmore ?><span class="circle-arrow"></span></a>
-                        <?php endif; ?> 
-                    </div>
-                </div>
-                <?php if($featured_widget_on): ?>
-                <div class="c-4">
-                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("home_sidebar") ) : ?>
-                        <!-- All this stuff in here only shows up if you DON'T have any widgets active in this zone -->
-                        No Widget 
-                   <?php endif; ?>  
-               </div>    
-               <?php endif; ?>
-               
-        </div><!-- end wrap -->  
-        <div class="clearfix"></div>                
-    </div><!-- END featured -->
-    <?php endif; ?>
+    <div class="wrap">
+    	
     
-    <?php if($middle_area_on) :?>
-     <div id="container-middle">
+    <div id="content" class="home">
+    	
+        <div id="slider">
         <div class="wrap">
-        
-        <?php 
-        if(Data()->getMain('mi_home.content_section_source_switch') == 'mpost'){
-            $args = array( 'numberposts' => 3, 
-            				'order'=> 'DESC', 
-            				'orderby' => 'post_date', 
-            				'hide_empty'=>1,
-                            'suppress_filters'=>false );
-            if($homepage_cagetory_id) $args['category'] = $homepage_cagetory_id;
-            
-            $postslist = get_posts( $args );
-        }else if (Data()->getMain('mi_home.content_section_source_switch') == 'mpage'){
-            $args = array('include'=>Data()->getMain('mi_home.content_section_pages'), 
-            			  'number'=>3,
-                          'sort_column'=>'menu_order',
-                          'sort_order'=>'ASC');
-            $postslist = get_pages($args);
-        }   
-        ?> 
-        <?php if(!isset($postslist) || !is_array($postslist)){$postslist = array();} foreach ($postslist as $post) : setup_postdata($post); ?> 
-                <div class="c-4">
-                    <h3><?php the_title(); ?></h3>
-                    <h4><?php echo the_subtitle();//get_post_meta($post->ID, '_post_subtitle', true); ?></h4>
+         	
+            <div class="c-8">
+            	<div id="big-slider">
+                    <div class="slider-mask"></div>
+                    <ul id="slider-items">
+                        <li>
+                            <img src="assets/slider_asset__0000_.jpg" width="650" height="293" alt="Slider Image" title="" />
+                        </li>
+                        <li> 
+                           <img src="assets/slider_asset__0001_.jpg" width="650" height="293" alt="Slider Image" title="" />
+                        </li>
+                        <li>
+                           <img src="assets/slider_asset__0002_.jpg" width="650" height="293" alt="Slider Image" title="" />
+                        </li>
+                        <li>
+                            <img src="assets/slider_asset__0003_.jpg" width="650" height="293" alt="Slider Image" title="" />
+                        </li>
+                          <li>
+                            <img src="assets/slider_asset__0004_.jpg" width="650" height="293" alt="Slider Image" title="" />
+                        </li>
+                    </ul>
                     
-                    <?php if(has_post_thumbnail()): 
-                        $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'theme-gallery-photo');
-                    ?>
-                    <div class="mask">
-                        <a title="<?php the_title_attribute(); ?>" href="<?php echo $large_image_url[0]; ?>" rel="example_group">
-                            <span class="middle-frame-mask"></span>
-                            <?php the_post_thumbnail('theme-small-thumb'); ?>
-                        </a>    
+                    <!-- This list is populated by jQuery Cycle -->
+                    <div id="pagination-container">
+                        <span class="previous"></span>
+                        <span id="slider-pagination"></span>
+                        <span class="next"></span>
                     </div>
-                     <?php endif; ?>
-                    <p><?php echo the_excerpt();?></p>
-                    <a href="<?php the_permalink(); ?>" class="read-more"><?php echo $readmore ?><span class="circle-arrow"></span></a>
-                 </div>
-         <?php endforeach; ?>     
-                 
-        </div><!-- end wrap -->         
-    </div> <!-- end container-middle -->
-  <?php endif; ?>  
+                </div><!-- END big-slider -->
+            </div>
+            
+            <div class="c-4">
+                <div id="cat-slider">
+
+                    <img src="http://www.allergiesatschool.com/wp-content/themes/kids/images/opt-in.png" style="margin: 0 auto;"/>
+                    
+                   
+                    
+            	</div> <!-- end cat-slider -->
+            </div>
+               
+        </div><!-- end wrap -->
         
-</div><!-- div#content.home -->
-<?php get_footer(); ?>
+                <div id="slider-bg"></div><!-- END slider-bg // ljulja, togodan, vepak // pronunced by my son Luka :) -->
+            
+        </div><!-- end slider-->
+        
+      
+        <div class="wrap">
+               
+	            <div class="c-8">
+	                <div class="page">
+                        <h2>ABOUT THE SCHOOL</h2>
+                        <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
+                        <a class="read-more">Read More<span class="circle-arrow"></span></a>
+                    </div>
+	            </div>
+	      <div class="c-4">
+                    <h3>Get our book on Amazon!</h3>
+                    <!-- ! subheading  <h4>Insightful ideas on dealing with severe food alleriges</h4>-->
+                    
+	                      <a href="http://www.amazon.com/Allergies-School-awareness-life-threatening-ebook/dp/B008VKCNY2/ref=sr_1_6?ie=UTF8&amp;qid=1358497057&amp;sr=8-6&amp;keywords=allergies+at+school"><img class="alignnone size-full wp-image-124" title="Allergies At School Book on Amazon" src="http://www.allergiesatschool.com/wp-content/uploads/41mmtr130UL._BO2204203200_PIsitb-sticker-arrow-clickTopRight35-76_AA278_PIkin4BottomRight-5722_AA300_SH20_OU01_.jpg" alt="Our Book" width="150" height="150" style="margin-left: 50px;"/></a> 
+                   
+                    <p>Say something really clever here</p>
+                    <a class="read-more">Read More<span class="circle-arrow"></span></a>
+                 </div>   
+               
+        </div><!-- end wrap -->                  
+        </div><!-- END featured -->
+        
+    </div><!-- ! end content -->
+    </div><!-- ! end wrap -->
+    <?php get_footer(); ?>
